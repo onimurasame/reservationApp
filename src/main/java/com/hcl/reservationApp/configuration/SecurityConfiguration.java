31be530a73452241872fb2f565ac1d3ed1236558
onimurasame <onimurasame@gmail.com>
2015-11-22 11:19:24 -0800

    Configuration of H2 and H2 Console

---
+++ src/main/java/com/hcl/reservationApp/configuration/SecurityConfiguration.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.hcl.reservationApp.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author onimu
+ *
+ */
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+                .authorizeRequests().antMatchers("/console/**").permitAll();
+
+        httpSecurity.csrf().disable();
+        httpSecurity.headers().frameOptions().disable();
+    }
+
+}
